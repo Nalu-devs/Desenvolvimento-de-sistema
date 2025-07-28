16b5f672189df681513bea26a8b239a43f9960fd
Aluno.Etec <aula@etec.com>
2025-07-28 20:45:14 -0300

    aula 1

---
+++ aula1.java
@@ -0,0 +1,12 @@
+package aula1;
+
+public class Aula1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+    }
+    
+}
