1718fb4a341aeb601b17b181e96cdece43cf74be
Aluno.Etec <aula@etec.com>
2025-07-29 20:30:19 -0300

    exercicio de fixação length

---
+++ length2.java
@@ -0,0 +1,10 @@
+package projeto7;
+public class Projeto7 {
+    public static void main(String[] args) {
+        String nome, endereco;
+        nome = "Ana Lúcia";
+        endereco = "Rua Sargento Leoncio Ferreira dos Santos";
+        System.out.println("O nome tem: "+nome.length()+" letras");
+        System.out.println("O endereço tem: "+endereco.length()+" letras");
+    }    
+}
