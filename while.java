25fe024a5fed6e72edd30385703330736ef46a1b
Aluno.Etec <aula@etec.com>
2025-07-29 19:45:33 -0300

    vendo como o while se comporta no java

---
+++ while.java
@@ -0,0 +1,16 @@
+package projeto2;
+public class Projeto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x;
+        x = 0;
+        while (x<10){
+            System.out.println("X vale:"+ x);
+            x=x+1;
+        }
+    }
+    
+}
