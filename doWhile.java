8fedb357279922f76fedf51381ddecf64772a22e
Aluno.Etec <aula@etec.com>
2025-07-29 20:15:26 -0300

    vendo aplicação da função do . . . while no java

---
+++ doWhile.java
@@ -0,0 +1,15 @@
+package projeto4;
+public class Projeto4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x;
+        x=0;
+        do{
+            System.out.println("X vale: "+x);
+            x=x+1;
+        }while(x<10);
+        }
+    }
