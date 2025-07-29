539b8a50071e7d166442ad251b00a834e01a4be5
Aluno.Etec <aula@etec.com>
2025-07-29 20:18:17 -0300

    variavel não nativa do java String

---
+++ String.java
@@ -0,0 +1,13 @@
+package projeto5;
+public class Projeto5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String texto;
+        texto = "Olá mundo!";
+        System.out.println(texto);
+    }
+    
+}
