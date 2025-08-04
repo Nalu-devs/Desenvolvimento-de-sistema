49a8578a48439874eee836fcad94b81c47f56bc9
Aluno.Etec <aula@etec.com>
2025-08-04 19:42:27 -0300

    Usando nextLine para criar espaço digitado para string

---
+++ aula 3/line.java
@@ -0,0 +1,13 @@
+package projeto1;
+import java.util.Scanner;
+public class Projeto1 {
+    public static void main(String[] args) {
+        int valor;
+        String seuNome;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        seuNome = teclado.nextLine();
+        System.out.println("Olá " + seuNome + ".");
+    }
+    
+}
