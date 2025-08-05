e947710a864770c16da4ea705ed43348bc36079b
Aluno.Etec <aula@etec.com>
2025-08-05 20:39:09 -0300

    arruamndo para decidir se é ou não primo

--- aula 3/numero.java
+++ aula 3/numero.java
@@ -4,17 +4,25 @@ package verificanumero;
 import java.util.Scanner;
 public class VerificaNumero {
     public static void main(String[] args) {
-        int dado, i, j;
+        int dado, i, j, cont=0;
         Scanner num;
         System.out.println("Digite o numero que começaremos");
         num = new Scanner(System.in);
         dado = num.nextInt();
-        for(i=dado; i>0; i--){
+        for(i=dado; i>0; i=i-1){
                 if(i%2==0)
-                    System.out.println(i+" O número é par");
+                    System.out.print(i+" Número é par  - ");
                 else
-                    System.out.println(i+" O número é impar");
-                //Falta descobrir o numero primo
+                    System.out.print(i+" Número é impar  - ");
+                cont=0;
+                for(j=i-1; j>1; j=j-1){
+                    if(i%j==0)
+                        cont++;
+                }
+                if(cont==0)
+                    System.out.println(" Número é primo");//o println serve para pular linha
+                else
+                    System.out.println(" Número não é primo");
         }
     }  
 }
