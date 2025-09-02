b0dbdb2171d2432bb4b5d6a343fbb850fd26fbf7
Aluno.Etec <aula@etec.com>
2025-09-02 20:15:39 -0300

    usando logicva para criar um programa de vetores pares e impares

---
+++ aula 9/ParImpar.java
@@ -0,0 +1,47 @@
+package parimpar;
+import java.util.Scanner;
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner teclado;
+        teclado = new Scanner(System.in);
+        int tamanho, contPar=0, contImpar=0;
+        System.out.println("Digite o tamanho do vetor:");
+        tamanho = teclado.nextInt();
+        int[] numero = new int[tamanho];
+        System.out.println("Digite os número para o vetor:");
+        for(int i=0; i < tamanho; i++){
+            numero[i] = teclado.nextInt();
+            if(numero[i]%2==0){
+                contPar++;
+                
+            }
+            else{
+                contImpar++;
+            }
+        }
+        
+        int[] par = new int[contPar];
+        int[] impar = new int[contImpar];
+        int iPar=0, iImpar=0;
+        
+        for(int i=0; i < tamanho; i++){
+            if(numero[i]%2==0){
+                par[iPar] = numero[i];
+                iPar++;
+            }
+            else{
+                impar[iImpar] = numero[i];
+                iImpar++;
+            }
+        }
+        System.out.println("Pares: ");
+        for(int j=0; j < par.length; j++){
+            System.out.println(par[j]);
+        }
+        System.out.println("Impares: ");
+        for(int x=0; x <impar.length; x++){
+            System.out.println(impar[x]);
+        }
+    }
+    
+}
