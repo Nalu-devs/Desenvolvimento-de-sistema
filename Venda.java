967dc11de8f8466bc2c190d9334bfa8ff1b48674
Ana Lúcia <pordosol699@gmail.com>
2025-09-16 21:58:10 -0300

    Create Venda.java
    classe de Loja.java

---
+++ Venda.java
@@ -0,0 +1,41 @@
+package loja;
+public class Venda {
+private int codigo;
+private String descricao;
+private double valor;
+private double quantidade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+
+}
